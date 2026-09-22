427bcf018deddd83ee5296848930f58cfd2a242a
asaashraf <60381989+asaashraf@users.noreply.github.com>
2026-09-21 22:24:14 -0600

    Update buffer_pool_manager_test.cpp
    Enable BufferPoolManager project tests

--- test/buffer/buffer_pool_manager_test.cpp
+++ test/buffer/buffer_pool_manager_test.cpp
@@ -12,6 +12,7 @@
 
 #include "buffer/buffer_pool_manager.h"
 #include <cstdio>
+#include <cstring>
 #include <random>
 #include <string>
 #include "gtest/gtest.h"
@@ -20,7 +21,7 @@ namespace bustub {
 
 // NOLINTNEXTLINE
 // Check whether pages containing terminal characters can be recovered
-TEST(BufferPoolManagerTest, DISABLED_BinaryDataTest) {
+TEST(BufferPoolManagerTest, BinaryDataTest) {
   const std::string db_name = "test.db";
   const size_t buffer_pool_size = 10;
 
@@ -86,7 +87,7 @@ TEST(BufferPoolManagerTest, DISABLED_BinaryDataTest) {
 }
 
 // NOLINTNEXTLINE
-TEST(BufferPoolManagerTest, DISABLED_SampleTest) {
+TEST(BufferPoolManagerTest, SampleTest) {
   const std::string db_name = "test.db";
   const size_t buffer_pool_size = 10;
 
